f9090e9d0339ab10b13599978e30d3c5df6fc7d5
Nastasya Grigorchuk <91945406+whynastasya@users.noreply.github.com>
2023-05-01 14:58:16 +0300

    Initial commit

---
+++ UIKit-Marathon/UIKit-Marathon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit-Marathon
+//
+//  Created by nastasya on 01.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
